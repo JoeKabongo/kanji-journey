406cdf67c334e0a754d257eef1daf0fe3102758a
JoeKabongo <jonathankabongo23@gmail.com>
2025-06-28 21:55:22 -0700

    boilerplate and folder structure for frontend folder

---
+++ frontend/src/router/AppRouter.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+import HomePage from '../pages/HomePage'
+import NotFoundPage from '../pages/NotFoundPage'
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
