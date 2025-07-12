ce80e2bfd7c43416d46129dc0f14ea9ab203706c
JoeKabongo <jonathankabongo23@gmail.com>
2025-07-12 01:35:45 -0700

    Move shared interface into a shared folder

---
+++ shared/types/kanji-details.ts
@@ -0,0 +1,10 @@
+export interface KanjiDetails {
+  id: number
+  character: string
+  meanings: string[]
+  onyomi: string[]
+  kunyomi: string[]
+  jlpt_level: string
+  stroke_count: number
+  examples: string[]
+}
